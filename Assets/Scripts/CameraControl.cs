f7dbae78f940de5330514a1605e29d9cf75058df
Yif1999 <yiguanyaoyaofen@foxmail.com>
2023-08-03 13:45:37 +0800

    first archive

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraControl : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+}
---
+++ Assets/Scripts/PoseUpdate.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using Pose = RosMessageTypes.Std.Float32MultiArrayMsg;
+
+public class PoseUpdate : MonoBehaviour
+{
+    float damping = 5.0f;
+    Vector3 position;
+    Quaternion rotation;
+
+    void Start()
+    {
+        ROSConnection.GetOrCreateInstance().Subscribe<Pose>("/unity/pose", PoseSync);
+    }
+
+    void PoseSync(Pose msg)
+    {
+        position = new Vector3((float)msg.data[0], (float)msg.data[1], (float)msg.data[2]);
+        rotation = Quaternion.Euler(new Vector3((float)msg.data[3], (float)msg.data[4], (float)msg.data[5]));
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * damping);
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime*damping);
+    }
+}
