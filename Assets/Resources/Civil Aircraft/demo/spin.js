5e3c82d35b48624db462a00efd112fe529792467
Yif1999 <yiguanyaoyaofen@foxmail.com>
2023-09-15 22:33:06 +0800

    refactor & serialized data receive in

--- Assets/Resources/Civil Aircraft/demo/spin.js
+++
@@ -1,4 +0,0 @@
-﻿function Update()
-{
-transform.RotateAround(transform.position, transform.forward, Time.deltaTime * -360f);
-}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,30 +1,91 @@
 using System;
 using System.Net.Sockets;
 using UnityEngine;
+using Tutorial;
+using Google.Protobuf;
+using System.Net;
+using System.Threading;
+using System.Text;
 
 public class TestScript : MonoBehaviour
 {
+
+    static Socket client;
     // Start is called before the first frame update
     void Start()
     {
+
+        try
+        {
+             IPAddress pAddress = IPAddress.Parse("127.0.0.1");
+             IPEndPoint pEndPoint = new IPEndPoint(pAddress, 9999);
+             client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+             client.Connect(pEndPoint);
+             Debug.Log("连接成功");
+             //创建线程，执行读取服务器消息
+             Thread c_thread = new Thread(Received);
+             c_thread.IsBackground = true;
+             c_thread.Start();
+        }
+        catch (System.Exception)
+        {
+             Debug.Log("未能连接");
+        }
+        stream.Close();
+        client.Close();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         gameObject.transform.Rotate(0, 1, 0);
 
-        // stream.Write(data, 0, data.Length);  // 发送数据
-        //TcpClient client = new TcpClient("127.0.0.1", 8080);
-        //NetworkStream stream = client.GetStream();
+    }
 
-        byte[] data = new byte[1024];
-        //int bytes = stream.Read(data, 0, data.Length);  // 接收数据
-        //string response = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
-        //Debug.Log(response);
+    static void ListPeople(AddressBook addressBook)
+    {
+        foreach (Person person in addressBook.People)
+        {
+            Debug.Log("Person ID: " + person.Id);
+            Debug.Log("  Name: " + person.Name);
+            Debug.Log("  E-mail address: " + person.Email);
 
-        //stream.Close();
-        //client.Close();
+        }
+    }
+
+    public static void Received()
+    {
+        while (true)
+        {
+            try
+            {
+                byte[] buffer = new byte[1000];
+                int len = client.Receive(buffer);
+                if (len == 0) break;
+                AddressBook addressBook = AddressBook.Parser.ParseFrom(buffer,0,len);
+                Debug.Log("来自服务器：" + addressBook.People[0].Name);
+            }
+           catch (System.Exception)
+            {
+    
+                throw;
+            }
+
+        }
+    }
+
+    public static void close()
+    {
+        try
+        {
+            client.Close();
+            Debug.Log("关闭客户端连接");
+        }
+        catch (System.Exception)
+        {
+             Debug.Log("未连接");
+        }
     }
 }
